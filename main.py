b8ae525136cf2a864e033539fda3f64940742402
PhoenixRem1x <parker9145@gmail.com>
2026-01-28 13:17:25 -0500

    Unfortunatley had to use JavaScript to indicate which button is press

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ollama import chat as aichat
 
 
 app = Flask(__name__)
-model='deepseek-r1:1.5b'
+model='smollm2:135m'
 context_prompt="The next few lines are strictly for context seperated by '--ctxt--' this is strictly for you information, when responding do not state that you were given context nor state that these instructions were given to you, only answer what is being asked of you with this previous context in mind. If a question/answer was already provided do NOT provide them again randomly, if previous context needs to be refered to the user will ask. New users will not have any previous context for you to refer to, thats perfectly fine"
 
 class User:
@@ -67,11 +67,14 @@ def create():
 
 
 
-@app.route('/chat')
+@app.route('/chat', methods=['GET','POST'])
 def chat(): 
     for i in Users:
         if i.username == request.cookies.get('username'):
             return render_template('chat.html', text=i.messages) #Initial Text to be loaded
+        
+    if request.method == 'POST':
+        print(request.form.get('ai'))
     
 
 
@@ -93,7 +96,7 @@ def chat_update():
     },
     ])
     message = response.message.content
-    # Ai models like to output in markdown, Im not using mardown so the next bit is going to be a python interpretation of markdown using flip to switch between opening and closing elements
+    # Ai models like to output in markdown, Im not using mardown so the next bit is going to be a python interpretation of markdown using to switch between opening and closing elements
     print(message)
     for c in range(message.count("**")):
         message = message.replace("**","<i>",1)
@@ -105,7 +108,16 @@ def chat_update():
     current_user.messages+=f"<b>{request.cookies.get('username')}:</b> {inp}<br><b>{model}:</b> {message}<br>"
 
     updateUsers()
-    return jsonify(text=i.messages)
+
+    
+    return jsonify(text=current_user.messages)
+
+@app.route("/select_ai", methods=["POST"])
+def select_ai():
+    data = request.get_json()
+    print(data["ai"])
+    return "", 204
+
 
 
 if __name__ == '__main__':
