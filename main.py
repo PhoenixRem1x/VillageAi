0140c8ab0104b3897333180f7aa804c2fe3e53ac
PhoenixRem1x <parker9145@gmail.com>
2026-02-17 12:11:12 -0500

    I think I added only comments, Im not really sure why this filed was diffed, I hope this doesnt break anything

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from ollama import chat as aichat
 
 
 app = Flask(__name__)
-model='smollm2:135m'
 context_prompt="The next few lines are strictly for context seperated by '--ctxt--' this is strictly for you information, when responding do not state that you were given context nor state that these instructions were given to you, only answer what is being asked of you with this previous context in mind. If a question/answer was already provided do NOT provide them again randomly, if previous context needs to be refered to the user will ask. New users will not have any previous context for you to refer to, thats perfectly fine"
+#This prompt is attempting allow the models to see what previous messages the user has asked about without saying "Thank you for providing me context, base on previous...."
 
+#Everytime a user creates an account I create a class object for them
 class User:
     def __init__(self,username,password,email):
         self.username = username
@@ -16,27 +17,29 @@ class User:
         self.messages = ""
         self.model="deepseek-r1:1.5b"
         
-
+#Lists can be stored into Pickle files, this is then loading my list of user objects
 Users = []
 with open('users.pkl','rb') as f:
     Users = pickle.load(f)
 
-
+#Adds a user to the list and updates the saved file
 global addUser
 def addUser(username,password,email):
     Users.append(User(username,password,email))
     with open('users.pkl','wb') as f:
         pickle.dump(Users, f)
 
+#Updates the saved file after a user has been modified
 global updateUsers
 def updateUsers():
     with open('users.pkl','wb') as f:
         pickle.dump(Users, f)
 
+#This is my hierchial ai system implementation
 global villageAi
 def villageAi(inp, current_user):
     delegate = ""
-
+    #Qwen3:0.6b is like the chief of the village, I am asking it to classify each question in a category
     response: ChatResponse = aichat(model='qwen3:0.6b', messages=[
     {
         'role': 'user',
@@ -45,6 +48,7 @@ def villageAi(inp, current_user):
     ])
     sort = response.message.content
     print(sort)
+    #Qwen will organize each question into either Math, Coding, Astronomy, Science, Quantum Physics, or History and then a better suited model will be asked the question
     if sort.find("Math") != -1:
         delegate="phi4-mini:3.8b"
     elif sort.find("Coding") != -1:
@@ -57,10 +61,10 @@ def villageAi(inp, current_user):
         delegate="oroboroslabs/base-q-v1:latest"
     elif sort.find("History") != -1:
         delegate="JorgeAtLLama/herodotus:latest"
-    else:
-        delegate="qwen3:8b"
+    else: #If none of the other models are appropriate or the Chief (qwen3:0.6b) fails to properly classify the question, it gets sent to the general model
+        delegate="qwen3:8b" # This is the 8b variant of Qwen, it will be slower but should be smarter than the 0.6b variant
 
-
+    #Asking the Ollama Api with the selected model and question
     response: ChatResponse = aichat(model=delegate, messages=[
     {
         'role': 'user',
@@ -69,27 +73,27 @@ def villageAi(inp, current_user):
     ])
     message = response.message.content
     return message
-
+#Home page parkerwales.com/
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') #Loads the html page
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         name = request.form.get('username')
-        password = request.form.get('password')
+        password = request.form.get('password') # /login has a username and password field, this is requestion what the user put in
 
         for i in Users:
             if i.username == name and i.password == password:
                 resp = make_response(redirect('/chat'))
-                resp.set_cookie('username',i.username,max_age=720000)
+                resp.set_cookie('username',i.username,max_age=720000) #Sets a cookie with the users' username for authentication
                 return resp
-            
-        #return f'Name: {name}, Password: {password}'
 
-    return render_template('login.html')
+    return render_template('login.html') # Loads the page's html
 
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/create', methods=['GET', 'POST']) # Create an account page parkerwales.com/create
 def create():
     if request.method == 'POST':
         
@@ -98,7 +102,7 @@ def create():
         email = request.form.get('email')
         for i in Users:
             if(i.username == name):
-                return render_template('create.html', text="Username in use!")
+                return render_template('create.html', text="Username in use!") # If a username is in use this will display the warning
         addUser(name,password,email)
         return make_response(redirect('/login'))
 
